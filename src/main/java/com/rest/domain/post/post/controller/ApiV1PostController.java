7be688e85e62bf57aee675911d31f962d71149a2
socra167 <socra167@gmail.com>
2025-01-24 14:46:33 +0900

    docs: api note

--- src/main/java/com/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/rest/domain/post/post/controller/ApiV1PostController.java
@@ -22,5 +22,14 @@ public class ApiV1PostController { // PostController인데 API용으로 쓸 거�
 	@ResponseBody
 	public List<Post> getItems() {
 		return postService.getPosts();
+		// [ API ]
+		// GET posts	: 모든 글 조회
+		// GET posts/1	: 1번 글 조회
+		// POST posts	: 글 하나 등록
+		// DELETE posts	: 글 하나 삭제
+		// PUT posts	: 글 수정
+
+		// 행위(동사)는 HTTP 메서드를 사용하고 명사는 엔드포인트로 표현한다
+		// v1, v2, ...	: 기존 API를 사용하고 있는 사람들을 고려해서, 이전 버전을 남겨놓아야 한다
 	}
 }

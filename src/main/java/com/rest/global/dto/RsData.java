d0d8f8b63f5ad05fcd06ac63fecc1a5ab9876e77
socra167 <socra167@gmail.com>
2025-01-27 12:39:11 +0900

    docs: 제네릭 타입 안정성

--- src/main/java/com/rest/global/dto/RsData.java
+++ src/main/java/com/rest/global/dto/RsData.java
@@ -13,6 +13,7 @@ public class RsData<T> {
 	private String msg;
 	private T data; // // Object로 그냥 쓰는 것보다 제네릭(T)로 쓰는 게 낫다
 	// 제네릭 T로 설정하면 결국 Object로 바뀐다
+	// 제네릭을 사용하면 의도한 타입으로 형변환시키기 때문에 컴파일 시점에 에러를 알 수 있다
 
 	public RsData(String code, String msg) {
 		this(code, msg, null);

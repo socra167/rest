c18dd07bb3bd963451e41ad0476df680ce914bf0
socra167 <socra167@gmail.com>
2025-01-27 11:38:57 +0900

    feat: 글 수정 시에 ID는 null로 응답한다

--- src/main/java/com/rest/global/dto/RsData.java
+++ src/main/java/com/rest/global/dto/RsData.java
@@ -8,9 +8,9 @@ import lombok.Getter;
 public class RsData {
 	private String code;
 	private String msg;
-	private long id;
+	private Long id;
 
 	public RsData(String code, String msg) {
-		this(code, msg, 0);
+		this(code, msg, null);
 	}
 }

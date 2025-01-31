e0da4266e2850fb343133f44a2381290814a87ee
socra167 <socra167@gmail.com>
2025-01-31 11:17:20 +0900

    feat: validation을 만족하지 않는 요청에 대한 예외 처리

--- src/main/java/com/rest/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rest/global/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.rest.global.exception;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -25,5 +28,21 @@ public class GlobalExceptionHandler {
 
 		// 예외가 발생해서 여기서 처리되는 것이므로, AOP가 적용되지 않아 200으로 응답된다
 		// 따라서 여기에선 ResponseEntity를 사용한다
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<RsData<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+		String field = e.getBindingResult().getFieldError().getField();
+		String message = e.getBindingResult().getFieldError().getDefaultMessage();
+
+		return ResponseEntity
+			.status(HttpStatus.BAD_REQUEST)
+			.body(
+				new RsData<>(
+					"400-1",
+					field + " : " + message
+				)
+			);
 	}
 }
